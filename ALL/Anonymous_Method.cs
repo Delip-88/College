12e317764955cbe9ad6218b035390aa4b39567e1
Delip-88 <sthdelip88@gmail.com>
2024-09-11 06:41:59 +0545

    abynomous method

---
+++ ALL/Anonymous_Method.cs
@@ -0,0 +1,13 @@
+// using System;
+// public delegate void Print(int value);
+// class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         Print print = delegate (int val)
+//         {
+//             Console.WriteLine("Inside Anynomous method . Value : {0}", val);
+//         };
+//         print(100);
+//     }
+// }
